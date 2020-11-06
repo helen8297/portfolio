150555caadde0723222c4ccbc758ac9a1be8b8e6
Helen Kent <helen8297@gmail.com>
2020-11-06 12:02:08 +0000

    add hello

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Hellooooo</a>
+                    Welcome to <a href="https://nextjs.org">Hellooooo Helen</a>
                 </h1>
 
                 <p className={styles.description}>
